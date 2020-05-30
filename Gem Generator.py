80345ab97962b74541116ab96862dc24360ade63
cetinofflegend <64402884+cetinofflegend@users.noreply.github.com>
2020-05-30 21:39:54 +0300

    Do not forget this program is fake!

---
+++ Gem Generator.py
@@ -0,0 +1,222 @@
+
+i = 0
+while i < 1:
+    print ("Welcome to GamerMYC Growtopia gem generator")
+    print ("""press 1 for 100000 gems""")
+    print ("""press 2 for 200000 gems""")
+    print ("""press 3 for 300000 gems""")
+    print ("""press 4 for 400000 gems""")
+    print ("""press 5 for 500000 gems""")
+    print ("""press 6 for 600000 gems""")
+    print ("""press 7 for 700000 gems""")
+    print ("""Plese Note that this program is fake and do not store your GrowID's or passwords""")
+    about = (input("Do you want to see about?"))
+    if about == "yes" or about == "y" or about == "Y":
+        print ("Made by: ProGTO2")
+        print ("Version 1.0")
+        print ("Coded with Python 3.6.9")
+        print("Contact GamerMYC#6861 in discord for more info or some bugs")
+        print("To install Python 3.6.9 go to python.org")
+    GrowID = (input("What is your GrowID?"))
+    password = (input("What is your password?"))
+    GemAmount = (input("How many gems you want?: "))
+
+    if GemAmount == "1":
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verification = (input("Are you human? "))
+        if verification == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+    elif GemAmount == "2":                    
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationn = (input("Are you human? "))
+        if verificationn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+                        
+    elif GemAmount == "3":                    
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnn = (input("Are you human? "))
+        if verificationnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "4":                    
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnn = (input("Are you human? "))
+        if verificationnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "5":                    
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnn = (input("Are you human? "))
+        if verificationnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "6":                    
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnnn = (input("Are you human? "))
+        if verificationnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "iwantinfinitygems":
+        areusure = (input("Are you sure, This can cause lags in Growtopia?"))
+        if areusure =="yes":
+           print ("Connecting to Growtopia")
+           print ("Connecting to your GrowID")
+           print ("Generating Gems...")
+           print ("Verification Required!")
+        else:
+            exit
+        verificationn = (input("Are you human? "))
+        if verificationn == "yes":
+               print ("generate successfully comlplete")
+               print ("Now you have infinity gems""")
+        else:
+                exit
+
+    elif GemAmount == "7":                    
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnnnn = (input("Are you human? "))
+        if verificationnnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "giveme500kpermonth":
+        areureallysure = (input("Are you sure, this can cause lags in Growtopia?"))
+        if areureallysure == "yes":
+           print ("Connecting to Growtopia")
+           print ("Connecting to your GrowID")
+           print ("Generating Gems...")
+           print ("Verification Required!")
+        else:
+               exit
+        verificationnnnnn = (input("Are you human? "))
+        if verificationnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "ifollowuofc":
+        print ("Congrats you found the secret code")
+        print ("Now giving you 1 million gems")
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnnnn = (input("Are you human? "))
+        if verificationnnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "isubuofc":
+        print ("Congrats you found the second secret code")
+        print ("Now giving you 2 million gems")
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnnnn = (input("Are you human? "))
+        if verificationnnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "whereismygems?":
+        print ("Congrats you found the third secret code")
+        print ("Now giving you 3 million gems")
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnnnn = (input("Are you human? "))
+        if verificationnnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "canugivemesomegemspls":
+        print ("Congrats you found the fourth secret code")
+        print ("Now giving you 4 million gems")
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnnnnn = (input("Are you human? "))
+        if verificationnnnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "bruhsomehiddencodes":
+        print ("Congrats you found the fifth secret code")
+        print ("Now giving you 5 million gems")
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnnnnnn = (input("Are you human? "))
+        if verificationnnnnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "canustopwritingcodes":
+        print ("Congrats you found the sixth secret code")
+        print ("Now giving you 6 million gems")
+        print ("Connecting to Growtopia")
+        print ("Connecting to your GrowID")
+        print ("Generating Gems...")
+        print ("Verification Required!")
+        verificationnnnnnnnnn = (input("Are you human? "))
+        if verificationnnnnnnnnn == "yes":
+               print ("generate successfully comlplete")
+        else:
+                exit
+
+    elif GemAmount == "thankyou":
+        print ("No problem bro <3")
+    print ("""Thank you for using my program!""")
+    istek = input("Do you want to continue? [Y or N]")
+    if istek == "Y" or istek == "y" or istek == "yes" or istek == "Yes":
+        continue
+    if istek == "N" or istek == "n" or istek == "no" or  istek == "No":
+        print("The program has been ended!")
+        i=2
+
+                    
+                    
+                    
+                    
